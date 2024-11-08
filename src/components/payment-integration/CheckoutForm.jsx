425213b809393db2a2e4934136c605774bce187a
Nikhil Barot <nikhilbarot@Nikhils-MacBook-Air.local>
2024-11-08 17:48:13 +0530

    env-added-in-gitignore

--- src/components/payment-integration/CheckoutForm.jsx
+++ src/components/payment-integration/CheckoutForm.jsx
@@ -9,6 +9,8 @@ const CheckoutForm = () => {
   const [message, setMessage] = useState('');
   const [success, setSuccess] = useState(false);
 
+  // const clientSecret = import.meta.env.VITE_STRIPE_SECRET_KEY;
+
   useEffect(() => {
 
     if (!stripe || !elements) return;
@@ -84,6 +86,7 @@ const CheckoutForm = () => {
 
       // Handle shipping address updates
       request.on('shippingaddresschange', async (event) => {
+
         // Update total amount or handle shipping address change logic if necessary
         // event.updateWith({ status: 'success' }); // Always mark as success for this example
 
@@ -92,48 +95,56 @@ const CheckoutForm = () => {
         } else {
 
           // Perform server-side request to fetch shipping options
-          // const response = await fetch('/calculateShipping', {
-          //   data: JSON.stringify({
-          //     shippingAddress: event.shippingAddress
-          //   })
-          // });
-          // const result = await response.json();
+          const response = await fetch('/calculateShipping', {
+            data: JSON.stringify({
+              shippingAddress: event.shippingAddress
+            })
+          });
+          const result = await response.json();
       
-          // event.updateWith({
-          //   status: 'success',
-          //   shippingOptions: result.supportedShippingOptions,
-          // });
-
-          event.updateWith(
-            fetch('https://destiny-server-nhyk.onrender.com/calculateShipping', {
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/json',
-              },
-              body: JSON.stringify({
-                shippingAddress: event.shippingAddress
-              }),
-            })
-            .then(response => response.json())
-            .then(result => {
-              // Return the updated payment details
-              return {
-                status: 'success',
-                shippingOptions: result.supportedShippingOptions,
-                total: result.total,
-                displayItems: result.displayItems,
-              };
-            })
-            .catch(error => {
-              console.error(error);
-              return { status: 'fail' };
-            })
-          );
+          event.updateWith({
+            status: 'success',
+            shippingOptions: result.supportedShippingOptions,
+          });
 
         }
 
-
       });
+
+      // request.on('paymentmethod', async (ev) => {
+      //   // Confirm the PaymentIntent without handling potential next actions (yet).
+      //   const {paymentIntent, error: confirmError} = await stripe.confirmCardPayment(
+      //     clientSecret,
+      //     {payment_method: ev.paymentMethod.id},
+      //     {handleActions: false}
+      //   );
+      
+      //   if (confirmError) {
+      //     // Report to the browser that the payment failed, prompting it to
+      //     // re-show the payment interface, or show an error message and close
+      //     // the payment interface.
+      //     ev.complete('fail');
+      //   } else {
+      //     // Report to the browser that the confirmation was successful, prompting
+      //     // it to close the browser payment method collection interface.
+      //     ev.complete('success');
+      //     // Check if the PaymentIntent requires any actions and, if so, let Stripe.js
+      //     // handle the flow. If using an API version older than "2019-02-11"
+      //     // instead check for: `paymentIntent.status === "requires_source_action"`.
+      //     if (paymentIntent.status === "requires_action") {
+      //       // Let Stripe.js handle the rest of the payment flow.
+      //       const {error} = await stripe.confirmCardPayment(clientSecret);
+      //       if (error) {
+      //         // The payment failed -- ask your customer for a new payment method.
+      //       } else {
+      //         // The payment has succeeded -- show a success message to your customer.
+      //       }
+      //     } else {
+      //       // The payment has succeeded -- show a success message to your customer.
+      //     }
+      //   }
+      // });
+
     }
 
   }, [stripe, elements]);
